1f44f9f42b08e1c9bb2771bd5ef8989f18affb4e
Ignacio <Ignacio@Deus>
2015-03-13 13:37:36 -0600

    Primera Subida

---
+++ src/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyecto;
+
+/**
+ *
+ * @author Ignacio
+ */
+public class PrimerProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Este es el proyecto solo uno tiene que crearlo
+       System.out.println("Compartir :B");
+    }
+    
+}
